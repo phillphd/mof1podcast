a07969b2271971619610ca8dada729c904e3b787
patrick <patrick@whiteboard.is>
2016-06-21 20:56:39 -0400

    Adding Donny site

---
+++ wp-content/themes/launchframe/tmnt/README.md
@@ -0,0 +1,15 @@
+Launchframe 4 Static Build
+
+- Bootstrap (4)
+- A little dash of Ruby
+- Bower
+- NPM and Gulp
+- etc
+
+
+To get started, run `bundle`, then `./start`.
+
+Note: you should also install Composer, NPM, and RubyGems before you start. Each of these will be responsible for installing necessary things for PHP, NodeJS, and Ruby, respectively.
+
+---
+
---
+++ wp-content/themes/launchframe/tmnt/index.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Launchframe Static</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6228872/7452152/css/fonts.css" />
+        <link rel="stylesheet" href="assets/dist/css/application.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <header>
+            <?php include 'assets/images/mondo-info.svg'; ?>
+        </header>
+
+        <section>
+            <div class="container vp-xs-top-2 vp-xs-bottom-2">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>As we continue our month of nothing but Ninja Turtles, we are excited to announce that Mondo—known to be one of the leading authorities when it comes to pop culture art and collectibles—is at it again with a beautiful new print of the “Hero in a half-shell” by Tom Whalen!</h2>
+                        <p>The Donatello print—among many others—is part of the Info·Rama 3-man show, opening on Friday, June 24 from 7-10pm. The show will run June 24 - July 9, Tuesdays through Saturdays noon-6pm. Other artists included in the show are UK artist, Matt Taylor and Info·Rama founder Kevin Tong! <em>(All three artists will be in attendance at the show opener this Friday)</em></p>
+                    </div>
+                    <div class="col-md-6">
+                        <p>Founded in 2014, Info·Rama strives to create infographic posters that are meticulously researched, illustrated, printed, and delivered. All of their products are made in America in small quantities.</p>
+                        <p>Listen to the <em>exclusive interview</em> with all three artists on Friday’s episode of the Master of One Podcast (Mof1). Hear Tom, Matt, and Kevin give a behind the scenes look at some of the other pieces from the show, as well as what it’s like to put a gallery show together that perfectly marries information and art!</p>
+                        <p>Catch up on the whole month of Turtles (including interviews with comic artist Jon Sommariva and creator Kevin Eastman) on the Mof1 Archive, and we will see you in Austin for the gallery show!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="print-section" style="background-image: url('/assets/images/donatello_print.png');">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <img class="print-section__print" src="/assets/images/donatello_print.png" />
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="print-section__title">
+                                        <h1>Donatello</h1>
+                                        by Tom Whalen<br />
+                                        12" x 36"  6-color Screen Print
+                                    </div>
+                                    <div class="print-section__description">
+                                        <p class="text-center">The turtle that is commonly accepted to be the smartest of the group, Donatello—adorned with the purple bandanna—uses his brains and his bo staff to out-smart his foes. And oh yeah, let’s not forget, he “does machines.” Whalen brings back the visionary look of the Jim Henson Creature Shop as inspiration for this piece.</p>
+                                        <img class="print-section__print" style="margin: 15px 0 35px;" src="/assets/images/character_detail.png" />
+                                        <div class="row">
+                                            <div class="col-lg-7">
+                                                <p>No matter where you look on this poster your eyes will have a treat! Chuck full of facts, stats, and info about everyone’s favorite nerdy turtle as well as the film that brought the cultural icons to life!</p>
+                                                <p>It also features some of the key characters that played a roll in the 90’s film like the Foot Clan, Master Splinter, and everyone’s favorite hockey enthusiast/vigilante, Casey Jones.</p>
+                                            </div>
+                                            <div class="col-lg-5">
+                                                <img class="print-section__print" src="/assets/images/weapon_detail.png" />
+                                            </div>
+                                        </div>
+                                        <p>Now everyone knows you can’t have just one turtle print on your wall. That’s why Tom has created three other pieces as a perfect compliment to our purple-bandanna friend.</p>
+                                        <p>Each of the other turtles are presented in one of their other iconic iterations. Whether from the original comic book, the beloved 80’s cartoon, or the new (fan-adored) Nickelodeon version of our half-shell heroes, there is something for every turtle fan!</p>
+                                        <h2>"The Mondo crew was especially helpful with research and fact-checking on the posters for this show. Head of Creative Production Eric Garza is an uber-Turtles fan and was my go-to guy for all queries about the TMNT poster set. As a thank you for his knowledge-sharing, I honored Eric's request to render Donatello (his favorite Turtle) in his 1990s form."</h2>
+                                        <p class="signature">-Tom Whalen</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <p class="share-cta">Share this page</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="social-share-links bg-gold">
+                            <li><a href="#!"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                            <li><a href="#!"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#!"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                            <li><a href="#!"><i class="fa fa-reddit" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="http://mof1podcast.com" target="_blank"><?php include 'assets/images/mof1_logo.svg'; ?></a>
+                    </div>
+                </div>
+                <div class="row vm-xs-bottom-1">
+                    <div class="col-md-12">
+                        <ul class="social-links">
+                            <li><a href="https://itunes.apple.com/us/podcast/mof1-podcast/id1074500173?mt=2" target="_blank"><i class="fa fa-music" aria-hidden="true"></i></a></li>
+                            <li><a href="https://twitter.com/Mof1podcast" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                            <li><a href="https://www.facebook.com/mof1podcast" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="https://www.instagram.com/mof1podcast" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row vm-xs-bottom-1">
+                    <div class="col-md-12">
+                        &copy; 2016 Master of One, LLC.
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+                        *All artwork is property of Tom Whalen, Mondo, and Info·Rama. The Teenage Mutant Ninja Turtles&trade; are owned by Nickelodeon.&trade;<br />
+                        Images strictly used for promotional purposes only with permission from the artist and gallery. Go to the show... Buy your own... Support great art!
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="assets/dist/js/script.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
